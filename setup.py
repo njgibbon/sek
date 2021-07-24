9990b9cfda1bbcb051a2f756b437b74fd0aa84c3
njgibbon <njgibbon@outlook.com>
2021-07-25 00:55:17 +0100

    moving things around trying to get over pypy module import err

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='sek',
-    version='0.0.6',
+    version='0.0.9',
     author="Nick Gibbon",
-    packages=["src"],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'sek = src.main:main',
+            'sek = sek.main:main',
         ],
     }
 )
