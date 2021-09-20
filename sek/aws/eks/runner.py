5be2ea86d76a34599a16d44b946565eb34c9f0cc
njgibbon <njgibbon@outlook.com>
2021-09-20 01:58:34 +0100

    fix f8 noqa skips

--- sek/aws/eks/runner.py
+++ sek/aws/eks/runner.py
@@ -2,7 +2,7 @@ import time
 
 from ...core.runner import CoreRunner
 from .context import AWSEKSContext
-from .checks import *  # no qa: F403
+from .checks import *  # noqa
 
 
 class AWSEKSRunner(CoreRunner):
@@ -11,7 +11,7 @@ class AWSEKSRunner(CoreRunner):
         super().__init__(name)
         self.link = "https://github.com/njgibbon/sek/blob/main/checks/aws/eks/readme.md"
         self.context = AWSEKSContext(name)
-        for check_class in CoreCheck.__subclasses__():  # no qa: F405
+        for check_class in CoreCheck.__subclasses__():  # noqa
             self.checks.append(check_class(name, self.context))
         super().scan()
         self.time = time.time() - start_time
