74d46808296d12615dce05039a4407745de9476d
njgibbon <njgibbon@outlook.com>
2021-09-20 02:33:55 +0100

    added min py as 3.7

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     long_description='Live Cloud Resource Security Configuration Scanning.',
     url='https://github.com/njgibbon/sek',
     version='0.0.14',
+    python_requires=">=3.7",
     author="Nick Gibbon",
     packages=find_packages(),
     entry_points={
